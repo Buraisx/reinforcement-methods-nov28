fd7f48e6f28a9901ac3ebbb94247e7bb654e8ff1
Situ William <situ_william@yahoo.com>
2017-11-29 13:24:45 -0500

    added exceptions for 11-13

--- exercise1.rb
+++ exercise1.rb
@@ -1,16 +1,16 @@
 def ordinal_numbers(num)
 	check_num = num % 10
-	if check_num == 1
+	if check_num == 1 && num != 11
 		return "#{num}st"
-	elsif check_num == 2
+	elsif check_num == 2 && num != 12
 		return "#{num}nd"
-	elsif check_num == 3
+	elsif check_num == 3 && num != 13
 		return "#{num}rd"
 	else
 		return "#{num}th"
 	end
 end
 
-(1..50).each do |x|
+(1..100).each do |x|
 	puts ordinal_numbers(x)
 end
