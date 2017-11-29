d0f55cf8a258057fbe6a89d61d87e55872119291
Situ William <situ_william@yahoo.com>
2017-11-29 13:29:28 -0500

    added 2 digits checker

--- exercise1.rb
+++ exercise1.rb
@@ -1,16 +1,17 @@
 def ordinal_numbers(num)
 	check_num = num % 10
-	if check_num == 1 && num != 11
+	check_num2 = num % 100
+	if check_num == 1 && check_num2 != 11
 		return "#{num}st"
-	elsif check_num == 2 && num != 12
+	elsif check_num == 2 && check_num2 != 12
 		return "#{num}nd"
-	elsif check_num == 3 && num != 13
+	elsif check_num == 3 && check_num2 != 13
 		return "#{num}rd"
 	else
 		return "#{num}th"
 	end
 end
 
-(1..100).each do |x|
+(1..300).each do |x|
 	puts ordinal_numbers(x)
 end
